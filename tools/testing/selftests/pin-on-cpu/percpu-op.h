2d85bf70d9c36442e44d7579a85aeafad377cd91
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2017-12-13 17:13:53 -0500

    pin-on-cpu/selftests: Provide percpu_op API
    Introduce percpu-op.h API. It uses rseq internally as fast-path if
    invoked from the right CPU, else pin_on_cpu as slow-path if called
    from the wrong CPU or if rseq fails.
    
    This allows acting on per-cpu data from various CPUs transparently from
    user-space. Use-cases such as rebalancing memory across per-cpu memory
    pools, or migrating tasks for a user-space scheduler, are thus
    facilitated.
    
    The use from userspace is, e.g. for a counter increment:
    
        int cpu, ret;
    
        cpu = percpu_current_cpu();
        ret = percpu_addv(&data->c[cpu].count, 1, cpu);
        if (unlikely(ret)) {
             perror("percpu_addv");
             return -1;
        }
        return 0;
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    CC: Shuah Khan <shuah@kernel.org>
    CC: Russell King <linux@arm.linux.org.uk>
    CC: Catalin Marinas <catalin.marinas@arm.com>
    CC: Will Deacon <will.deacon@arm.com>
    CC: Thomas Gleixner <tglx@linutronix.de>
    CC: Paul Turner <pjt@google.com>
    CC: Peter Zijlstra <peterz@infradead.org>
    CC: Andy Lutomirski <luto@amacapital.net>
    CC: Andi Kleen <andi@firstfloor.org>
    CC: Dave Watson <davejwatson@fb.com>
    CC: Chris Lameter <cl@linux.com>
    CC: Ingo Molnar <mingo@redhat.com>
    CC: "H. Peter Anvin" <hpa@zytor.com>
    CC: Ben Maurer <bmaurer@fb.com>
    CC: Steven Rostedt <rostedt@goodmis.org>
    CC: "Paul E. McKenney" <paulmck@linux.vnet.ibm.com>
    CC: Josh Triplett <josh@joshtriplett.org>
    CC: Linus Torvalds <torvalds@linux-foundation.org>
    CC: Andrew Morton <akpm@linux-foundation.org>
    CC: Boqun Feng <boqun.feng@gmail.com>
    CC: linux-kselftest@vger.kernel.org
    CC: linux-api@vger.kernel.org

---
+++ tools/testing/selftests/pin-on-cpu/percpu-op.h
@@ -0,0 +1,247 @@
+/* SPDX-License-Identifier: LGPL-2.1 OR MIT */
+/*
+ * percpu-op.h
+ *
+ * (C) Copyright 2017-2018 - Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
+ */
+
+#ifndef PERCPU_OP_H
+#define PERCPU_OP_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <syscall.h>
+#include <unistd.h>
+#include <linux/sched.h>
+#include "rseq.h"
+
+static inline uint32_t percpu_current_cpu(void)
+{
+	return rseq_current_cpu();
+}
+
+static inline int sys_pin_on_cpu(int cmd, int flags, int cpu)
+{
+	return syscall(__NR_pin_on_cpu, cmd, flags, cpu);
+}
+
+static inline void pin_on_cpu_set(int cpu)
+{
+	int ret;
+
+	ret = sys_pin_on_cpu(PIN_ON_CPU_CMD_SET, 0, cpu);
+	if (ret)
+		abort();
+}
+
+static inline void pin_on_cpu_clear(void)
+{
+	int ret;
+
+	ret = sys_pin_on_cpu(PIN_ON_CPU_CMD_CLEAR, 0, 0);
+	if (ret)
+		abort();
+}
+
+static inline __attribute__((always_inline))
+int percpu_fence(int cpu)
+{
+	if (cpu < 0)
+		return -1;
+	if ((uint32_t) cpu == percpu_current_cpu())
+		return 0;
+	/*
+	 * Temporarily pinning to the target CPU acts as a rseq fence
+	 * for that CPU.
+	 */
+	pin_on_cpu_set(cpu);
+	pin_on_cpu_clear();
+	return 0;
+}
+
+static inline __attribute__((always_inline))
+int percpu_cmpeqv_storev(intptr_t *v, intptr_t expect, intptr_t newv,
+			 int cpu)
+{
+	int ret;
+
+	ret = rseq_cmpeqv_storev(v, expect, newv, cpu);
+check:
+	if (rseq_unlikely(ret)) {
+		if (ret > 0)
+			return ret;
+		pin_on_cpu_set(cpu);
+		ret = rseq_cmpeqv_storev(v, expect, newv, percpu_current_cpu());
+		pin_on_cpu_clear();
+		goto check;
+	}
+	return 0;
+}
+
+static inline __attribute__((always_inline))
+int percpu_cmpnev_storeoffp_load(intptr_t *v, intptr_t expectnot,
+			       off_t voffp, intptr_t *load, int cpu)
+{
+	int ret;
+
+	ret = rseq_cmpnev_storeoffp_load(v, expectnot, voffp, load, cpu);
+check:
+	if (rseq_unlikely(ret)) {
+		if (ret > 0)
+			return ret;
+		pin_on_cpu_set(cpu);
+		ret = rseq_cmpnev_storeoffp_load(v, expectnot, voffp, load,
+						 percpu_current_cpu());
+		pin_on_cpu_clear();
+		goto check;
+	}
+	return 0;
+}
+
+static inline __attribute__((always_inline))
+int percpu_addv(intptr_t *v, intptr_t count, int cpu)
+{
+	int ret;
+
+	ret = rseq_addv(v, count, cpu);
+check:
+	if (rseq_unlikely(ret)) {
+		pin_on_cpu_set(cpu);
+		ret = rseq_addv(v, count, percpu_current_cpu());
+		pin_on_cpu_clear();
+		goto check;
+	}
+	return 0;
+}
+
+static inline __attribute__((always_inline))
+int percpu_cmpeqv_storev_storev(intptr_t *v, intptr_t expect,
+				intptr_t *v2, intptr_t newv2,
+				intptr_t newv, int cpu)
+{
+	int ret;
+
+	ret = rseq_cmpeqv_trystorev_storev(v, expect, v2, newv2,
+					   newv, cpu);
+check:
+	if (rseq_unlikely(ret)) {
+		if (ret > 0)
+			return ret;
+		pin_on_cpu_set(cpu);
+		ret = rseq_cmpeqv_trystorev_storev(v, expect, v2, newv2,
+						   newv, percpu_current_cpu());
+		pin_on_cpu_clear();
+		goto check;
+	}
+	return 0;
+}
+
+static inline __attribute__((always_inline))
+int percpu_cmpeqv_storev_storev_release(intptr_t *v, intptr_t expect,
+					intptr_t *v2, intptr_t newv2,
+					intptr_t newv, int cpu)
+{
+	int ret;
+
+	ret = rseq_cmpeqv_trystorev_storev_release(v, expect, v2, newv2,
+						   newv, cpu);
+check:
+	if (rseq_unlikely(ret)) {
+		if (ret > 0)
+			return ret;
+		pin_on_cpu_set(cpu);
+		ret = rseq_cmpeqv_trystorev_storev_release(v, expect, v2, newv2,
+							   newv,
+							   percpu_current_cpu());
+		pin_on_cpu_clear();
+		goto check;
+	}
+	return 0;
+}
+
+static inline __attribute__((always_inline))
+int percpu_cmpeqv_cmpeqv_storev(intptr_t *v, intptr_t expect,
+				intptr_t *v2, intptr_t expect2,
+				intptr_t newv, int cpu)
+{
+	int ret;
+
+	ret = rseq_cmpeqv_cmpeqv_storev(v, expect, v2, expect2, newv, cpu);
+check:
+	if (rseq_unlikely(ret)) {
+		if (ret > 0)
+			return ret;
+		pin_on_cpu_set(cpu);
+		ret = rseq_cmpeqv_cmpeqv_storev(v, expect, v2, expect2,
+						newv, percpu_current_cpu());
+		pin_on_cpu_clear();
+		goto check;
+	}
+	return 0;
+}
+
+static inline __attribute__((always_inline))
+int percpu_cmpeqv_memcpy_storev(intptr_t *v, intptr_t expect,
+				void *dst, void *src, size_t len,
+				intptr_t newv, int cpu)
+{
+	int ret;
+
+	ret = rseq_cmpeqv_trymemcpy_storev(v, expect, dst, src, len,
+					   newv, cpu);
+check:
+	if (rseq_unlikely(ret)) {
+		if (ret > 0)
+			return ret;
+		pin_on_cpu_set(cpu);
+		ret = rseq_cmpeqv_trymemcpy_storev(v, expect, dst, src, len,
+						   newv, percpu_current_cpu());
+		pin_on_cpu_clear();
+		goto check;
+	}
+	return 0;
+}
+
+static inline __attribute__((always_inline))
+int percpu_cmpeqv_memcpy_storev_release(intptr_t *v, intptr_t expect,
+					void *dst, void *src, size_t len,
+					intptr_t newv, int cpu)
+{
+	int ret;
+
+	ret = rseq_cmpeqv_trymemcpy_storev_release(v, expect, dst, src, len,
+						   newv, cpu);
+check:
+	if (rseq_unlikely(ret)) {
+		if (ret > 0)
+			return ret;
+		pin_on_cpu_set(cpu);
+		ret = rseq_cmpeqv_trymemcpy_storev_release(v, expect, dst, src, len,
+							   newv, percpu_current_cpu());
+		pin_on_cpu_clear();
+		goto check;
+	}
+	return 0;
+}
+
+static inline __attribute__((always_inline))
+int percpu_deref_loadoffp(intptr_t *p, off_t voffp, intptr_t *load, int cpu)
+{
+	int ret;
+
+	ret = rseq_deref_loadoffp(p, voffp, load, cpu);
+check:
+	if (rseq_unlikely(ret)) {
+		if (ret > 0)
+			return ret;
+		pin_on_cpu_set(cpu);
+		ret = rseq_deref_loadoffp(p, voffp, load, percpu_current_cpu());
+		pin_on_cpu_clear();
+		goto check;
+	}
+	return 0;
+}
+
+#endif  /* PERCPU_OP_H_ */
